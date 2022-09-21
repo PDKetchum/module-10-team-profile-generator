60fc65885bf8df832d86f0e60fab5bec273722f8
PDKetchum <pxiong265@gmail.com>
2022-09-20 20:02:40 -0500

    fixed functions in index.js and added readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # module-10-team-profile-generator
+
+## Overview
+
+To generate a webpage that displays my team's basic info, I created an application to have quick access to their emails and GitHub profiles
+The application takes in user input and inserts it into a template which then generates the index.html file.
+
+## Installation:
+
+1. `git clone git@github.com:PDKetchum/module-10-team-profile-generator.git`
+2. Open `index.js`
+
+## How to use:
+
+1. `npm install`
+2. `node index.js`
+3. Input team information
+
+GitHub URL: https://github.com/PDKetchum/module-10-team-profile-generator
+
+![](screenshot.png)
+_Screenshot of page_
--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-//WHEN I start the application
-//THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-
 async function promptForManager() {
   return inquirer.prompt([
     {
@@ -43,7 +40,7 @@ async function promptForEngineer(engineer) {
     },
     {
       type: "input",
-      name: "EngineerEmployeeId",
+      name: "id",
       message: "Enter the Engineer's employee ID",
     },
     {
@@ -109,8 +106,6 @@ async function init() {
 
   let hasMoreEmployee = true;
 
-  console.log("Im Here");
-
   while (hasMoreEmployee) {
     const selectionAnswer = await promptForMoreEmployee();
     if (selectionAnswer.selection === "No more employees") {
@@ -123,6 +118,7 @@ async function init() {
         engineerAnswer.email,
         engineerAnswer.github
       );
+      engineers.push(engineer);
     } else if (selectionAnswer.selection === "Intern") {
       const internAnswer = await promptForIntern();
       const intern = new Intern(
@@ -131,8 +127,111 @@ async function init() {
         internAnswer.email,
         internAnswer.school
       );
+      interns.push(intern);
     }
   }
+
+  const htmlContent = generateHtmlPage(manager, engineers, interns);
+  writeToFile("index.html", htmlContent);
 }
 
 init();
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("Successfully created index.html!")
+  );
+}
+
+function generateHtmlPage(manager, engineers, interns) {
+  return `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
+    />
+  </head>
+  <body>
+    <section>
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4 text-center">My Team</h1>
+        </div>
+      </div>
+    </section>
+    <section class="d-flex justify-content-center flex-wrap">
+      <div class="card text-bg-light mb-3 m-3" style="max-width: 18rem">
+        <div class="card-header">
+          <h4>${manager.getName()}</h4>
+          <h5>${manager.getRole()}</h5>
+        </div>
+        <div class="card-body">
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${manager.getId()}</li>
+            <li class="list-group-item">Email: Email: <a href="mailto${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+          </ul>
+        </div>
+       </div>
+      ${generateEmployeeHtml(engineers, interns)}
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  </body>
+</html>
+  `;
+}
+
+function generateEmployeeHtml(engineers, interns) {
+  let addEmployeesinfo = "";
+  for (let i = 0; i < engineers.length; i++) {
+    const newEngineer = generateHtmlEngineer(engineers[i]);
+    addEmployeesinfo += newEngineer;
+  }
+  for (let i = 0; i < interns.length; i++) {
+    const newIntern = generateHtmlIntern(interns[i]);
+    addEmployeesinfo += newIntern;
+  }
+  return addEmployeesinfo;
+}
+
+function generateHtmlEngineer(engineer) {
+  return `
+  <div class="card text-bg-light mb-3 m-3" style="max-width: 18rem">
+    <div class="card-header">
+      <h4>${engineer.getName()}</h4>
+      <h5>${engineer.getRole()}</h5>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${engineer.getId()}</li>
+        <li class="list-group-item">Email: <a href="mailto${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+        <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+      </ul>
+    </div>
+  </div>
+  `;
+}
+
+function generateHtmlIntern(intern) {
+  return `
+  <div class="card text-bg-light mb-3 m-3" style="max-width: 18rem">
+    <div class="card-header">
+      <h4>${intern.getName()}</h4>
+      <h5>${intern.getRole()}</h5>
+    </div>
+    <div class="card-body">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: ${intern.getId()}</li>
+        <li class="list-group-item">Email: Email: <a href="mailto${intern.getEmail()}">${intern.getEmail()}</a></li>
+        <li class="list-group-item">School: ${intern.getSchool()}</li>
+      </ul>
+    </div>
+  </div>
+  `;
+}

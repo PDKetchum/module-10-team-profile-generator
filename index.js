ae66a1ba3fe3c7cb719b327f5373d6dc8420e0c4
PDKetchum <pxiong265@gmail.com>
2022-09-21 17:20:43 -0500

    added html page title

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ function generateHtmlPage(manager, engineers, interns) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>My Team</title>
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css"
